ab208bc2e1d600f1ee9cdd245f72b52caecdca51
shamal melantha <shamalmelantha3@gmail.com>
2024-06-13 11:08:20 +0530

    update server

--- server.js
+++ server.js
@@ -28,13 +28,10 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
   })
-  .catch((err) => console.error(err));
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
